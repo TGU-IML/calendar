c5f5131d4637a36b11af2a312fb9cbcd3b980c1a
shootk <shoot.k.uta.7@gmail.com>
2021-02-24 03:51:15 +0900

    カレンダーを月曜日始まりに変更 カレンダー表をスクラップボックス用に作成 旧カレンダーの表示も残した

--- calendar_list.js
+++ calendar_list.js
@@ -5,16 +5,14 @@ var endOfMonth = new Date(today.getFullYear(), today.getMonth()+1, 0);
 var endOfMonthDate = endOfMonth.getDate();
 //曜日変換用配列
 dayTable = [ '日', '月', '火', '水', '木', '金', '土'];
-spaceTable = [']&nbsp;&nbsp;|' ,
-']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[',
- ']&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[',
-  ']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[',
-   ']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[',
-    ']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[',
-     ']&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[']
-var link_head = ' https://scrapbox.io/IML/'
-//何周目かの初期値を入れるため，月初に設定
-var weekNum = 0;
+spaceTable = [']&nbsp;&nbsp;|<br>' ,
+']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;',
+ ']&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;',
+  ']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;',
+   ']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;',
+    ']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;',
+     ']&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;']
+var link_head = '&nbsp;https://scrapbox.io/IML/'
 //書き込むドキュメントを開く
 document.open();
 //以下、日付と対応した書き込みを行う
@@ -22,20 +20,27 @@ document.open();
 
 today.setDate(1);
 document.write(
-'[/ 　　　　　　　　'+today.getFullYear()+'/'+today.getMonth()+1
-+']<br>'
-+'|&nbsp;[[Mon]]&nbsp;|&nbsp;&nbsp;[[Tue]]&nbsp;|&nbsp;[[Wed]]&nbsp;|&nbsp;&nbsp;[[Thu]]&nbsp;|&nbsp;&nbsp;&nbsp;[[Fri]]&nbsp;&nbsp;|&nbsp;&nbsp;[[Sat]]&nbsp;&nbsp;|&nbsp;&nbsp;[[Sun]]&nbsp;|' + '<br>');
+'[/ 　　　　　　　　'+today.getFullYear()+'/' + (today.getMonth() + 1)+'<br>'
++'|&nbsp;[[Mon]]&nbsp;|&nbsp;&nbsp;[[Tue]]&nbsp;|&nbsp;[[Wed]]&nbsp;|&nbsp;&nbsp;[[Thu]]&nbsp;|&nbsp;&nbsp;&nbsp;[[Fri]]&nbsp;&nbsp;|&nbsp;&nbsp;[[Sat]]&nbsp;&nbsp;|&nbsp;&nbsp;[[Sun]]&nbsp;|<br>');
 
 if(dayTable[today.getDay()] != '月'){
-    var lastDay = new Date();
-    lastDay.setDate(-((today.getDay + 6) % 7));
-    var endOfLastMonthDate = new Date(lastDay.getFullYear(), lastDay.getMonth()+1, 0);;
-    document.write('|   ');
+    var lastDay = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    lastDay.setDate(1-((today.getDay() + 6) % 7));
+    var endOfLastMonthDate = new Date(lastDay.getFullYear(),(lastDay.getMonth() + 1), 0).getDate();
+    document.write('|&nbsp;&nbsp;&nbsp;');
     for(i = lastDay.getDate(); i <= endOfLastMonthDate; i++){
         lastDay.setDate(i);
         document.write(
-            i + link_head + lastDay.getFullYear() + '/' + lastDay.getMonth() + '/'  + lastDay.getDate() + spaceTable[lastDay.getDay()]
-        );
+            "["+ ("0" + i).slice(-2) + link_head + lastDay.getFullYear() + '/' + (lastDay.getMonth() + 1) + '/'  + lastDay.getDate() + spaceTable[lastDay.getDay()]
+        );}
+}
+for(i=1; i<=endOfMonthDate;i++){
+    today.setDate(i);
+    if(dayTable[today.getDay()] == '月'){
+        document.write('|&nbsp;&nbsp;&nbsp;');
     }
+    document.write(
+        "["+("0" + i).slice(-2) + link_head + today.getFullYear() + '/' + (today.getMonth() + 1) + '/'  + today.getDate() + spaceTable[today.getDay()]
+        );
 }
 document.close();
