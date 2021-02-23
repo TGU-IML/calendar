5d04f818504bbe419af12e18fba2510bb576bb8d
shootk <shoot.k.uta.7@gmail.com>
2021-02-24 02:41:18 +0900

    ok

--- calendar_list.js
+++ calendar_list.js
@@ -4,9 +4,15 @@ var today = new Date();
 var endOfMonth = new Date(today.getFullYear(), today.getMonth()+1, 0);
 var endOfMonthDate = endOfMonth.getDate();
 //曜日変換用配列
-dayTable = [ "日", "月", "火", "水", "木", "金", "土"];
-spaceTable = ["]  |" ,"]  |   [", "]   |   [", "]  |   [", "]  |   [", "]  |   [", "]   |   ["]
-var link_head = " https://scrapbox.io/IML/"
+dayTable = [ '日', '月', '火', '水', '木', '金', '土'];
+spaceTable = [']&nbsp;&nbsp;|' ,
+']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[',
+ ']&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[',
+  ']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[',
+   ']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[',
+    ']&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[',
+     ']&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;[']
+var link_head = ' https://scrapbox.io/IML/'
 //何周目かの初期値を入れるため，月初に設定
 var weekNum = 0;
 //書き込むドキュメントを開く
@@ -16,25 +22,20 @@ document.open();
 
 today.setDate(1);
 document.write(
-"|                                "
-+"[/ "+today.getFullYear()+"/"+today.getMonth()+"]"
-+"	                             |"
-+"| [[Mon]] |  [[Tue]] | [[Wed]] |  [[Thu]] |   [[Fri]]  |  [[Sat]]  |  [[Sun]] |" + "<br>");
+'[/ 　　　　　　　　'+today.getFullYear()+'/'+today.getMonth()+1
++']<br>'
++'|&nbsp;[[Mon]]&nbsp;|&nbsp;&nbsp;[[Tue]]&nbsp;|&nbsp;[[Wed]]&nbsp;|&nbsp;&nbsp;[[Thu]]&nbsp;|&nbsp;&nbsp;&nbsp;[[Fri]]&nbsp;&nbsp;|&nbsp;&nbsp;[[Sat]]&nbsp;&nbsp;|&nbsp;&nbsp;[[Sun]]&nbsp;|' + '<br>');
 
-if(dayTable[today.getDay()] != "月"){
-    var lastMonday = new Date();
-    lastMonday.setDate(-((today.getDay + 6) % 7))
-    for(i = -((today.getDay + 7) % 7); i < 1; i++){
-        document.write();
+if(dayTable[today.getDay()] != '月'){
+    var lastDay = new Date();
+    lastDay.setDate(-((today.getDay + 6) % 7));
+    var endOfLastMonthDate = new Date(lastDay.getFullYear(), lastDay.getMonth()+1, 0);;
+    document.write('|   ');
+    for(i = lastDay.getDate(); i <= endOfLastMonthDate; i++){
+        lastDay.setDate(i);
+        document.write(
+            i + link_head + lastDay.getFullYear() + '/' + lastDay.getMonth() + '/'  + lastDay.getDate() + spaceTable[lastDay.getDay()]
+        );
     }
 }
-
-document.write(
-    "|   [" + 
-    01 +
-    link_head +
-    "]  |   "+
-    [[[02 https://scrapbox.io/IML/2021/2/23]]]   |   [[[03 https://scrapbox.io/IML/2021/2/23]]]  |   [[[04 https://scrapbox.io/IML/2021/2/24]]]  |   [[[05 https://scrapbox.io/IML/2021/2/24]]]  |   [[[06 https://scrapbox.io/IML/2021/2/24]]]   |   [[[07 https://scrapbox.io/IML/2021/2/24]]]  |
-);
-
 document.close();
